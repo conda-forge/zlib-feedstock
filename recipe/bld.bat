03e3406f4be00f33d084969fc64a4df029d980a2
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2022-05-27 12:19:54 +0300

    Move building wapi to bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,32 @@ set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%;%RECIPE_DIR%
 
 :: Configure.
+:: -DZLIB_WINAPI switches to WINAPI calling convention. See Q7 in DLL_FAQ.txt.
+cmake -G "NMake Makefiles" ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+      -D CMAKE_C_FLAGS="-DZLIB_WINAPI " ^
+      %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build %SRC_DIR% --config Release
+if errorlevel 1 exit 1
+
+:: Test.
+ctest
+if errorlevel 1 exit 1
+
+:: Copy built zlib.dll with the same name provided by http://www.winimage.com/zLibDll/
+:: This is needed for example for cuDNN
+:: https://docs.nvidia.com/deeplearning/cudnn/install-guide/index.html#install-zlib-windows
+copy "zlib.dll" "%LIBRARY_BIN%\zlibwapi.dll" || exit 1
+copy "zlib.lib" "%LIBRARY_LIB%\zlibwapi.lib" || exit 1
+
+
+:: Now build regular zlib.
+:: Configure.
 cmake -G "NMake Makefiles" ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
@@ -11,7 +37,7 @@ cmake -G "NMake Makefiles" ^
 if errorlevel 1 exit 1
 
 :: Build.
-cmake --build %SRC_DIR% --target INSTALL --config Release
+cmake --build %SRC_DIR% --target INSTALL --config Release --clean-first
 if errorlevel 1 exit 1
 
 :: Test.
