f9bafc5e7714f5991b1c916bc7b6eeefdd91e2ed
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2022-06-13 10:10:15 +0300

    Return 1 if compile flags is wrong

--- recipe/test_compile_flags.c
+++ recipe/test_compile_flags.c
@@ -5,4 +5,14 @@ int main()
     // compile flags must be larger than 1 << 10 if compiled with ZLIB_WINAPI
     printf("zlib version %s = 0x%04x, compile flags = 0x%lx\n",
             ZLIB_VERSION, ZLIB_VERNUM, zlibCompileFlags());
+
+#ifdef ZLIB_WINAPI
+    if (zlibCompileFlags() & (1 << 10) == 0) {
+        exit(1);
+    }
+#else
+    if (zlibCompileFlags() & (1 << 10) != 0) {
+        exit(1);
+    }
+#endif
 }
