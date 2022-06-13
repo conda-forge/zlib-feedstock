5cfd856176e4dba197719fae2938d8aaee02ce4d
Isuru Fernando <isuruf@gmail.com>
2022-06-13 15:30:20 -0500

    Fix test

--- recipe/test_compile_flags.c
+++ recipe/test_compile_flags.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    // compile flags must be larger than 1 << 10 if compiled with ZLIB_WINAPI
+    // compile flags must have the 10-th bit set if compiled with ZLIB_WINAPI
     printf("zlib version %s = 0x%04x, compile flags = 0x%lx\n",
             ZLIB_VERSION, ZLIB_VERNUM, zlibCompileFlags());
 
 #ifdef ZLIB_WINAPI
-    if (zlibCompileFlags() < (1 << 10)) {
+    if ((zlibCompileFlags() & (1 << 10)) == 0) {
         exit(1);
     }
 #else
-    if (zlibCompileFlags() >= (1 << 10)) {
+    if ((zlibCompileFlags() & (1 << 10)) != 0) {
         exit(1);
     }
 #endif
