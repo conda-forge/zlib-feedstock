e76359ac9527bd1f077a673cf1ca066b404c704d
H. Vetinari <h.vetinari@gmx.com>
2024-09-08 15:10:00 +0200

    use upstream LICENSE file
    https://github.com/madler/zlib/blob/v1.3.1/LICENSE

--- recipe/build.sh
+++ recipe/build.sh
@@ -22,6 +22,3 @@ make install
 
 # Remove man files.
 rm -rf $PREFIX/share
-
-# Copy license file to the source directory so conda-build can find it.
-cp $RECIPE_DIR/license.txt $SRC_DIR/license.txt
