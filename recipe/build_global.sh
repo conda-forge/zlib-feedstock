c8baaae0fa3406b1e5ae608707e704505085eb95
H. Vetinari <h.vetinari@gmx.com>
2024-09-10 10:37:20 +0200

    add emscripten as a target

--- recipe/build_global.sh
+++ recipe/build_global.sh
@@ -11,6 +11,14 @@ mkdir -p $RECIPE_DIR/ignored
 mkdir build
 cd build
 
+if [[ ${cross_target_platform} == emscripten-* ]]; then
+    # derived from zlib-recipe in emscripten-forge, see
+    # https://github.com/emscripten-forge/recipes/blob/989e8506ffaa3a24dccd49733c4454fadaf34b02/recipes/recipes_emscripten/zlib
+    export CMAKE_ARGS="$CMAKE_ARGS -DZLIB_BUILD_EXAMPLES=OFF"
+    # https://github.com/emscripten-core/emscripten/issues/15276
+    export CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_PROJECT_INCLUDE=$RECIPE_DIR/overwriteProp.cmake"
+fi
+
 cmake -G Ninja \
     ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
