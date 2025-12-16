436ea71842a1f060fbf2e9b7b994a9876010b52c
Michał Górny <mgorny@gentoo.org>
2025-12-16 19:17:49 +0100

    Revert "Try what happens if BUILD_SHARED_LIBS is set"
    This reverts commit 842b73542f51e79236097abb02779da4a0dad2fc.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -49,7 +49,6 @@ cmake -G "NMake Makefiles" ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
       -D INSTALL_PKGCONFIG_DIR=%LIBRARY_PREFIX%\lib\pkgconfig ^
-      -D BUILD_SHARED_LIBS=ON ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
