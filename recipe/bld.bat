842b73542f51e79236097abb02779da4a0dad2fc
Michał Górny <mgorny@gentoo.org>
2025-12-16 19:09:29 +0100

    Try what happens if BUILD_SHARED_LIBS is set
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -49,6 +49,7 @@ cmake -G "NMake Makefiles" ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
       -D INSTALL_PKGCONFIG_DIR=%LIBRARY_PREFIX%\lib\pkgconfig ^
+      -D BUILD_SHARED_LIBS=ON ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
